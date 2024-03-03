6e3b8dcdcd1803a8ebdf6ee31dc9afdd8c8fdb38
JuliaJPereira <juliapereira.odc@gmail.com>
2024-03-03 11:55:19 -0300

    Criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-// import { Despesas } from './Despesas';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Despesas } from './Despesas';
 import { LoginComponent } from './Login';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginComponent />,
+  },
+  {
+    path: '/despesas',
+    element: <Despesas />,
+  },
+]);
+
 function App() {
-  // return <Despesas />;
-  return <LoginComponent />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
